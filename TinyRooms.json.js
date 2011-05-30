3ff0b188ff75a871c2b8fb277af063449cadb302
Eric Conner <econner@stanford.edu>
2011-05-30 13:24:30 -0700

    Added tinyrooms json file.

---
+++ TinyRooms.json.js
@@ -0,0 +1,31 @@
+[
+   {
+      "id": "1",
+      "name": "Outside building",
+      "description": "You are standing at the end of a road before a small brick building. A small stream flows out of the building and down a gully to the south. A road runs up a small hill to the west.",
+      "motions": [
+         {"dir": "WEST", "room": "2"},
+         {"dir": "UP", "room": "2"},
+         {"dir": "NORTH", "room": "3"},
+         {"dir": "IN", "room": "3"}
+      ]
+   },
+   {
+      "id": "2",
+      "name": "End of road",
+      "description": "You are at the end of a road at the top of a small hill. You can see a small building in the valley to the east.",
+      "motions": [
+         {"dir": "DOWN", "room": "1"},
+         {"dir": "EAST", "room": "1"}
+      ]
+   },
+   {
+      "id": "3",
+      "name": "Inside building",
+      "description": "You are inside a building, a well house for a large spring.",
+      "motions": [
+         {"dir": "SOUTH", "room": "1"},
+         {"dir": "OUT", "room": "1"}
+      ]
+   }
+]
