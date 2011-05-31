d65d43910aaa2de6c4e403af832d42a3e80b3215
Eric Conner <econner@stanford.edu>
2011-05-30 21:24:01 -0700

    Add styling and comments to adv objects.

--- AdvObject.js
+++ AdvObject.js
@@ -1,3 +1,10 @@
+/*
+* File: AdvObject.js
+* 
+* Definition of an adventure game object.  This object contains just a 
+* few fields, an identifying name, a verbose description, and an initial
+* room number in which to place the item.
+*/
 function AdvObject(obj) {
    this.name = obj.name;
    this.description = obj.description;
@@ -5,5 +12,6 @@ function AdvObject(obj) {
 }
 
 AdvObject.prototype.toString = function() {
+   // print the description with a span so colors can be added
    return "<span class='item'>" + this.description + "</span>"
 }
