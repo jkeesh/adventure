f4f233f6bd15f680c8497a973e13674bfaf451cf
Jeremy Keeshin <jkeesh@gmail.com>
2011-05-31 17:06:22 -0700

    add files for adventure person

---
+++ AdvPerson.js
@@ -0,0 +1,17 @@
+/*
+* File: AdvPerson.js
+* 
+* Definition of an adventure person. An adventure person has a name,
+* a room number that they start in, and a quote that they say. An 
+* adventure person can be prompted to speak his quote with the command
+* TALK
+*/
+function AdvPerson(obj) {
+   this.name = obj.name;
+   this.quote = obj.quote;
+   this.initialRoomId = obj.initialLoc;
+}
+
+AdvPerson.prototype.talk = function(){
+    return "<span class='quote'>" + this.quote + "-" + this.name +"</span>";
+}
---
+++ SmallPeople.json.js
@@ -0,0 +1,17 @@
+[
+   {
+      "name": "Justice Black",
+      "quote": "Only a free and unrestrained press can effectively expose deception in government. And paramount among the responsibilities of a free press is the duty to prevent any part of the government from deceiving the people and sending them off to distant lands to die of foreign fevers and foreign shot and shell.",
+      "initialLoc": "1"
+   },
+   {
+      "name": "Daniel Ellsberg",
+      "description": "I felt that as an American citizen, as a responsible citizen, I could no longer cooperate in concealing this information from the American public. I did this clearly at my own jeopardy and I am prepared to answer to all the consequences of this decision.",
+      "initialLoc": "2"
+   },
+   {
+      "name": "John Perry Barlow",
+      "description": "The first serious infowar is now engaged. The field of battle is WikiLeaks. You are the troops.",
+      "initialLoc": "3"
+   }
+]
